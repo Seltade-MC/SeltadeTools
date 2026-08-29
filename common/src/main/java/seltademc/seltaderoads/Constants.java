118da831b0ba84111c55dd94beddf969b7e8f3f1
Dyxux <dyxux@vivaldi.net>
2026-08-29 03:23:20 +0200

    Refactor mod structure

---
+++ common/src/main/java/seltademc/seltaderoads/Constants.java
@@ -0,0 +1,11 @@
+package seltademc.seltaderoads;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "seltaderoads";
+	public static final String MOD_NAME = "SeltadeRoads";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/seltademc/seltaderoads/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package seltademc.seltaderoads.platform;
+
+import seltademc.seltaderoads.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
