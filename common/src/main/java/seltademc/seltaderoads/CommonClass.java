fb5634a52cf4763a163e1bb6544414f437b11817
Dyxux <dyxux@vivaldi.net>
2026-08-29 12:05:39 +0200

    Remove some hardcoded variables

--- common/src/main/java/seltademc/seltaderoads/CommonClass.java
+++ common/src/main/java/seltademc/seltaderoads/CommonClass.java
@@ -23,9 +23,8 @@ public class CommonClass {
         // your own abstraction layer. You can learn more about this in our provided services class. In this example
         // we have an interface in the common code and use a loader specific implementation to delegate our call to
         // the platform specific approach.
-        if (Services.PLATFORM.isModLoaded("seltaderoads")) {
-
-            Constants.LOG.info("Hello to seltaderoads");
+        if (Services.PLATFORM.isModLoaded(Constants.MOD_ID)) {
+            Constants.LOG.info("Hello to {}", Constants.MOD_ID);
         }
     }
 }
--- common/src/main/java/seltademc/seltaderoads/mixin/MixinMinecraft.java
+++ common/src/main/java/seltademc/seltaderoads/mixin/MixinMinecraft.java
@@ -13,7 +13,7 @@ public class MixinMinecraft {
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) {
         
-        Constants.LOG.info("This line is printed by an example mod common mixin!");
+        Constants.LOG.info("This line is printed by {} common mixin!", Constants.MOD_ID);
         Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
     }
 }
